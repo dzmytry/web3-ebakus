a739e5f1c66890af7cc58a8cde146248438612e8
Chris Ziogas <ziogaschr@gmail.com>
2018-12-27 10:39:51 +0200

    Add support in Cryptonight WebAssembly for node.js promises for module loading

---
+++ hash_cn/webassembly/module-post.js
@@ -0,0 +1,23 @@
+// Credits to AnthumChris https://gist.github.com/AnthumChris/c1b5f5526b966011dac39fbb17dacafe
+// Module.ready resolves when WASM instantiates. (ready is now a property and not function via @surma fork)
+Module.ready = new Promise(function(resolve, reject) {
+  addOnPreMain(function() {
+    var api = {
+      cryptonight: Module.cwrap('cryptonight', '', [
+        /*'number', 'number', 'number', 'number', 'number'*/
+      ]),
+      Malloc: Module._malloc,
+    };
+    resolve(api);
+  });
+
+  // Propagate error to Module.ready.catch()
+  // WARNING: this is a hack based Emscripten's current abort() implementation
+  // and could break in the future.
+  // Rewrite existing abort(what) function to reject Promise before it executes.
+  var origAbort = this.abort;
+  this.abort = function(what) {
+    reject(Error(what));
+    origAbort.call(this, what);
+  };
+});
