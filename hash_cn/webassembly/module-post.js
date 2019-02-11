e3d205fe557132dcf405e512bfe59358aa20a69e
Chris Ziogas <ziogaschr@gmail.com>
2019-02-11 17:58:44 +0200

    Apply prettier

--- hash_cn/webassembly/module-post.js
+++ hash_cn/webassembly/module-post.js
@@ -1,15 +1,15 @@
 // Credits to david-polak https://github.com/kripken/emscripten/issues/5820#issuecomment-385722568
 Module['ready'] = new Promise(function(resolve, reject) {
-  delete Module['then'];
+  delete Module['then']
   Module['onAbort'] = function(what) {
-    reject(what);
-  };
+    reject(what)
+  }
   addOnPostRun(function() {
     Module['cryptonight'] = Module.cwrap('cryptonight', '', [
       /*'number', 'number', 'number', 'number', 'number'*/
-    ]);
-    Module['Malloc'] = Module._malloc;
+    ])
+    Module['Malloc'] = Module._malloc
 
-    resolve(Module);
-  });
-});
+    resolve(Module)
+  })
+})
