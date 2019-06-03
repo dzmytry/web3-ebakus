d30d9dd88e5276bab5508f1dd0243d92e232a68f
Chris Ziogas <ziogaschr@gmail.com>
2019-06-03 12:14:44 +0300

    Use worker_threads on node for cryptonight

--- src/node/calculateWorkNonce.js
+++ src/node/calculateWorkNonce.js
@@ -1,70 +1,100 @@
 import clz from 'clz-buffer'
+import { parentPort } from 'worker_threads'
 import Module from '../common/cryptonight'
 import { hex2uint8 } from '../common/utils'
 
-export default new Promise(function(resolve, reject) {
-  Module.ready.then(api => {
-    function getCryptoNightBigEndian(input, output) {
-      api.cryptonight(
-        output.byteOffset,
-        input.byteOffset,
-        input.byteLength,
-        /* lite */ 0,
-        /* variant */ 2
-      )
+let currentWorkNonce = 0
 
-      // reverse from little-endian to big-endian
-      output.reverse()
+const throttled = (delay, fn) => {
+  let lastCall = 0
+  return function(...args) {
+    const now = new Date().getTime()
+    if (now - lastCall < delay) {
+      return
     }
+    lastCall = now
+    return fn(...args)
+  }
+}
 
-    // calculate a cryptonight hash
-    function calculateWorkNonce(hash, targetDifficulty) {
-      let currentWorkNonce = 0
+const mainThreadUpdate = throttled(500, () => {
+  // emit the final workNonce calculated for transaction
+  parentPort.postMessage({
+    cmd: 'current',
+    workNonce: currentWorkNonce,
+  })
+})
 
-      let bits = Math.log2(targetDifficulty)
-      bits = Math.ceil(bits)
-      const target = bits
+Module.ready.then(api => {
+  function getCryptoNightBigEndian(input, output) {
+    api.cryptonight(
+      output.byteOffset,
+      input.byteOffset,
+      input.byteLength,
+      /* lite */ 0,
+      /* variant */ 2
+    )
 
-      const heap = Module.HEAPU8.buffer
-      const input = new Uint8Array(heap, api.Malloc(64), 64)
+    // reverse from little-endian to big-endian
+    output.reverse()
+  }
 
-      const rlpIntArray = hex2uint8(heap, hash, api.Malloc)
-      const rlpHash = new Uint8Array(heap, api.Malloc(32), 32)
-      getCryptoNightBigEndian(rlpIntArray, rlpHash)
+  // calculate a cryptonight hash
+  function calculateWorkNonce(hash, targetDifficulty) {
+    let currentWorkNonce = 0
 
-      input.set(rlpHash, 0)
+    let bits = Math.log2(targetDifficulty)
+    bits = Math.ceil(bits)
+    const target = bits
 
-      const inputDataView = new DataView(
-        heap,
-        input.byteOffset,
-        input.byteLength
-      )
+    const heap = Module.HEAPU8.buffer
+    const input = new Uint8Array(heap, api.Malloc(64), 64)
 
-      const outputMalloc = api.Malloc(32)
-      let bestBit = 0
-      do {
-        // set in big-endian
-        inputDataView.setUint32(60, currentWorkNonce)
+    const rlpIntArray = hex2uint8(heap, hash, api.Malloc)
+    const rlpHash = new Uint8Array(heap, api.Malloc(32), 32)
+    getCryptoNightBigEndian(rlpIntArray, rlpHash)
 
-        const outputHash = new Uint8Array(heap, outputMalloc, 32)
-        getCryptoNightBigEndian(input, outputHash)
+    input.set(rlpHash, 0)
 
-        const firstBit = clz(outputHash)
+    const inputDataView = new DataView(heap, input.byteOffset, input.byteLength)
 
-        if (firstBit > bestBit) {
-          bestBit = firstBit
+    const outputMalloc = api.Malloc(32)
+    let bestBit = 0
+    do {
+      // set in big-endian
+      inputDataView.setUint32(60, currentWorkNonce)
 
-          if (bestBit >= target) {
-            break
-          }
+      const outputHash = new Uint8Array(heap, outputMalloc, 32)
+      getCryptoNightBigEndian(input, outputHash)
+
+      const firstBit = clz(outputHash)
+
+      if (firstBit > bestBit) {
+        bestBit = firstBit
+
+        if (bestBit >= target) {
+          break
         }
+      }
 
-        currentWorkNonce++
-      } while (bestBit <= target)
+      currentWorkNonce++
 
-      return currentWorkNonce
-    }
+      mainThreadUpdate()
+    } while (bestBit <= target)
+  }
 
-    resolve(calculateWorkNonce)
+  parentPort.postMessage({
+    cmd: 'ready',
+  })
+
+  parentPort.on('message', data => {
+    const { hash, targetDifficulty } = data
+    calculateWorkNonce(hash, targetDifficulty)
+
+    // emit the final workNonce calculated for transaction
+    parentPort.postMessage({
+      cmd: 'finished',
+      workNonce: currentWorkNonce,
+    })
   })
 })
